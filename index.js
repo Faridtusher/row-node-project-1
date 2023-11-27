dc0a2322fcda062eaa5fcb63f3cbe535aa61a6e9
farid123d <117964049+farid123d@users.noreply.github.com>
2023-11-28 07:04:57 +0800

    create-storage-template

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const http = require('http')
 const environment = require('./helpers/environment')
 const {handelReqRes} = require('./helpers/handelReqRes')
+const data = require('./lib/data')
 
 // scaffolding
 const app ={};
@@ -19,6 +20,19 @@ app.handelReqRes = handelReqRes;
 
 // run the server
 app.createServer();
+
+// testing
+data.update('myFile', 'newFile', { Name: "Dima && Farid in sha allah husband and wife", country: "Bangladesh && jordan and palestine" }, (err) =>{
+   console.log('Error was',err)  
+})
+
+data.create('test', 'newFile', { Name: " Farid", country: "Bangladesh " }, (err) => {
+   console.log('Error was', err)
+})
+
+// data.delete('test', 'newFile', (err) => {
+//    console.log('Error was', err)
+// })
 
 // exports
 module.exports = app;

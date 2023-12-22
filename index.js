47b7acdbf21d663eb72e195b7989b08d7484b456
farid123d <117964049+farid123d@users.noreply.github.com>
2023-12-22 11:45:13 +0800

    update-workers

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 // // dependencies
-const http = require('http')
-const environment = require('./helpers/environment')
-const {handelReqRes} = require('./helpers/handelReqRes')
-const twilio = require('./helpers/notification')
+const server = require('./lib/server')
+const worker = require('./lib/workers')
 
 // scaffolding
-const app ={};
-
-// do the twilio
-twilio.sendMsgTwilio('01164374797', 'I love Almighty', (err) =>{
-   console.log(`This is the error ${err}`)
-})
-
-
+const app = {};
 
 // create the server
-app.createServer = () =>{
-   const server = http.createServer(app.handelReqRes)
-   server.listen(environment.port, () =>{
-      console.log(`Your server is running in ${environment.port}`)
-   })
+app.init = () =>{
+   server.init();
+
+   worker.init();
+
 }
 
-// handel request and response
-app.handelReqRes = handelReqRes;
-
-// run the server
-app.createServer();
+// run the app
+app.init();
 
 // exports
 module.exports = app;
